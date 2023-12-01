74a4e138286ab0c5c50087630c444fdbb1a34122
Shrit Shah <shritshah2113@gmail.com>
2023-11-30 20:48:29 -0500

    Added ufw Firewall controls till 3.5.1.7

--- OS_Hardening.sh
+++ OS_Hardening.sh
@@ -40,8 +40,8 @@ clear
 
 
 echo -e "$W"; figlet -t Ubuntu OS Hardening; echo -e "$NC"
-echo -e "\t\t\e[31;1m:::::: \e[0;7mBy: Shrit Shah & Tarun Kalyani${NC} \e[31;1m::::::${NC}"
-echo -e "\e[31;1m--- ${NC}This is a menu based automation script to harden Ubuntu Linux 22.04 LTS based on CIS Benchmarks v8 ${NC} \e[31;1m---${NC}"
+echo -e "\t\e[31;1m:::::: \e[0;7mBy: Shrit Shah & Tarun Kalyani${NC} \e[31;1m::::::${NC}"
+echo -e "\n\e[31;1m--- ${NC}This is a menu based automation script to harden Ubuntu Linux 22.04 LTS based on CIS Benchmarks v8 ${NC} \e[31;1m---${NC}"
 
 end()
 {
@@ -60,7 +60,7 @@ partition()
         echo -e "\n${C}[${W}*${C}] Creating Partition for $1... \n${NC}"
         echo -e "${C}[${W}*${C}] Available Disks: ${NC}"
         fdisk -l | grep "Disk /dev/sd"
-        printf "${C}[${W}+${C}] ${W}Enter the disk name to partition [/dev/sdX]: ${NC}"
+        echo -e "${C}[${W}+${C}] ${W}Enter the disk name to partition [/dev/sdX]: ${NC}"
         read disk_name
         echo -e "n\np\n\n\n+5G\n\nw" | fdisk $disk_name &>> /dev/null
         fdisk -l | grep $disk_name
@@ -70,7 +70,7 @@ partition()
         else
             echo -e "${G}[${W}^${G}] ${BG}Partition Created Successfully${NC}"
         fi
-        printf "${C}[${W}+${C}] ${W}Enter the partition name [e.g. /dev/sdb1]: ${NC}"
+        echo -e "${C}[${W}+${C}] ${W}Enter the partition name [e.g. /dev/sdb1]: ${NC}"
         read part_name
         mkfs.ext4 $part_name &>> /dev/null
         echo "$part_name $1 ext4 defaults,${2},${3},${4} 0 2 " >> /etc/fstab
@@ -87,14 +87,14 @@ partition()
 
 while [ 0 ]
 do
-    echo -e "\v----------${BOLD} 1. Initial Setup Configuration${NC}----------"
-    echo -e "\v\t${C}[${W}1${C}] Seperate Partitions:/tmp, /var, /var/tmp, /var/log, /var/log/audit & /home${Y}"
-    echo -e "\t${C}[${W}2${C}] Disable USB and automounting ${Y}"
-    echo -e "\t${C}[${W}3${C}] Software Patches & Upgrades ${Y}"
-    echo -e "\t${C}[${W}4${C}] Unattended Auto Download and Installation of latest stable packages${Y}"
+    echo -e "\v----------${BOLD} 1. Initial Setup: System Configuration${NC}----------"
+    echo -e "\v\t${C}[${W}1${C}] Seperate Partitions:/tmp, /var, /var/tmp, /var/log, /var/log/audit & /home${NC}"
+    echo -e "\t${C}[${W}2${C}] Disable USB and automounting ${NC}"
+    echo -e "\t${C}[${W}3${C}] Software Patches & Upgrades ${NC}"
+    echo -e "\t${C}[${W}4${C}] Unattended Auto Download and Installation of latest stable packages${NC}"
     echo -e "\t${C}[${W}0${C}] ${Y}Back to Main Menu${NC}\n"
 
-    printf "${C}[${W}+${C}] Select section: ${NC}" 
+    echo -e "${C}[${W}+${C}] Select section: ${NC}" 
     read menu_opt
 
     case $menu_opt in 
@@ -127,12 +127,12 @@ do
             ;;
         3)  
             echo -e "${C}[${W}*${C}] ${W}Updating Packages...${NC}"
-            apt update && apt upgrade -y
+            apt update &>> /dev/null && apt upgrade -y &>> /dev/null
             if [ $? -ne 0 ]
             then
                 echo -e "${R}[${W}!${R}] ${BR}${blink}Error Updating Packages${NC}"
             else
-                echo -e "${G}[${W}^${G}] ${BG}Packages Updated Successfully${NC}"
+                echo -e "${G}[${W}^${G}] ${BG}Repositories Refreshed & Packages Updated${NC}"
             fi
             ;;
         4)  
@@ -143,7 +143,7 @@ do
                 echo -e "${R}[${W}!${R}] ${BR}${blink}Error Installing unattended-upgrades${NC}"
             else
                 echo -e "${G}[${W}^${G}] ${BG}unattended-upgrades Installed Successfully${NC}"
-                dpkg-reconfigure -plow unattended-upgrades &>> /dev/null
+                dpkg-reconfigure -plow unattended-upgrades 2>> /dev/null
                 if [ $? -ne 0 ]
                 then
                     echo -e "${R}[${W}!${R}] ${BR}${blink}Error Configuring unattended-upgrades${NC}"
@@ -163,17 +163,133 @@ do
 done
 }
 
-Section-2: Services
+#Section-2: Services
 services()
 {
-    echo -e "${Y}[${W}-${Y}] ${BW}So far the Script is implmented for CIS Controls in IG1. And the ${BOLD}2. Services${NC}${BW} section of the Benchmarks is not a part of IG-1 ${NC}"
+    echo -e "${Y}[${W}-${Y}] ${BY}So far the Script is implmented for CIS Controls in IG1. And the ${UL}2.Services${NC}${BY} section of the Benchmarks is not a part of IG-1.${NC}"
+    echo -e "${Y}[${W}-${Y}] ${BY}So, this section is not yet implemented in the script.${NC}"
 }
 
 #Section-3: Network Configuration
-#net_config()
-#{
+net_config()
+{
 
-#}
+conflict_check()
+{
+systemctl status $1 &>> /dev/null
+    if [ $? -ne 4 ]
+    then
+        echo -e "${C}[${W}*${C}] ${W}${1} found in systems, it may conflict with ufw.${NC}"
+        echo -e "${C}[${W}+${C}] Do you want to remove ${1} to avoid conflict [Y/N]: ${NC}"
+        read conflict_opt
+        if [ $conflict_opt == "Y" ] || [ $conflict_opt == "y" ] || [ $conflict_opt == "yes" ] || [ $conflict_opt == "Yes" ]
+        then
+            echo -e "${C}[${W}*${C}] ${W}Removing ${1}...${NC}"
+            apt remove ${1} -y &>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Removing ${1}${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}${1} Removed Successfully${NC}"
+            fi
+        else
+            echo -e "${C}[${W}*${C}] ${R}As you wish! Don't come to me later if something breaks.${NC}"
+        fi
+    fi
+}
+
+while [ 0 ]
+do
+    echo -e "\v----------${BOLD} 3. Network Configuration${NC}----------"
+    echo -e "\v\t${C}[${W}1${C}] Installing and Enabling Uncomplicated Firewall (UFW)${NC}"
+    echo -e "\t${C}[${W}2${C}] Firewall: Block traffic from loopback that is not from loopback interface.${NC}"
+    echo -e "\t${C}[${W}3${C}] Firewall: Allow outbound connections by default${NC}"
+    echo -e "\t${C}[${W}4${C}] Firewall: Deny inbound connections by default except server open ports.${NC}"
+    echo -e "\t${C}[${W}5${C}] View Firewall Rules${NC}"
+    echo -e "\t${C}[${W}0${C}] ${Y}Back to Main Menu${NC}\n"
+
+    echo -e "${C}[${W}+${C}] Select section: ${NC}" 
+    read menu_opt
+
+    case $menu_opt in 
+        1) 
+            echo -e "${C}[${W}*${C}] ${W}Installing UFW...${NC}"
+            apt install ufw -y &>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Installing UFW${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}UFW Installed Successfully${NC}"
+                
+                conflict_check iptables-persistent
+                conflict_check firewalld
+                conflict_check nftables
+                
+                echo -e "${C}[${W}*${C}] ${W}Enabling UFW...${NC}"
+                ufw enable &>> /dev/null
+                if [ $? -ne 0 ]
+                then
+                    echo -e "${R}[${W}!${R}] ${BR}${blink}Error Enabling UFW${NC}"
+                else
+                    echo -e "${G}[${W}^${G}] ${BG}UFW Enabled Successfully${NC}"
+                fi
+            fi
+            ;;
+        2)  
+            echo -e "${C}[${W}*${C}] ${W}Config: Allow all traffic on the loopback interface while Block traffic from loopback that is not from loopback interface...${NC}"
+            ufw allow in on lo 2>> /dev/null
+            ufw deny in from 127.0.0.0/8 2>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Configuring Loopback${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}Loopback Configured Successfully${NC}"
+            fi
+            ;;
+        3)  
+            echo -e "${C}[${W}*${C}] ${W}Config: Allow all outbound connections by default...${NC}"
+            ufw default allow outgoing &>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Configuring Outbound Connections${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}Outbound Connections Configured Successfully${NC}"
+            fi
+            ;;
+        4)  
+            echo -e "${C}[${W}+${C}] ${W}Enter the port numbers to allow inbound connections [e.g. 22,80,443]: ${NC}"
+            read port_num
+            echo -e "${C}[${W}*${C}] ${W}Config: Allow inbound connections on ports: ${port_num}...${NC}"
+            ufw allow $port_num proto tcp &>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Configuring Inbound Connections${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}Inbound Connections Configured Successfully${NC}"
+            fi
+            echo -e "${C}[${W}*${C}] ${W}Config: Deny all inbound connections by default...${NC}"
+            ufw default deny incoming &>> /dev/null
+            if [ $? -ne 0 ]
+            then
+                echo -e "${R}[${W}!${R}] ${BR}${blink}Error Configuring Inbound Connections${NC}"
+            else
+                echo -e "${G}[${W}^${G}] ${BG}Inbound Connections Configured Successfully${NC}"
+            fi
+            ;;
+        5)
+            echo -e "${C}[${W}*${C}] ${W}Viewing Firewall Rules...${NC}"
+            ufw status verbose
+            ;;
+        0) 
+            echo -e "${C}[${W}*${C}] ${R}Exiting to Main Menu...${NC}"
+            break
+            ;;
+        *)
+            echo -e "${R}[${W}!${R}] ${BR}${blink}Select valid option from the menu${NC}"
+            ;;
+    esac
+done
+}
 
 #Section-4: Logging & Auditing
 #log_audit()
@@ -197,15 +313,15 @@ services()
 while [ 0 ]
 do
     echo -e "\v----------${BOLD}CIS Benchmarks Sections${NC}----------"
-    echo -e "\v\t${C}[${W}1${C}] Initial Setup ${Y}"
-    echo -e "\t${C}[${W}2${C}] Services ${Y}"
-    echo -e "\t${C}[${W}3${C}] Network Configuration ${Y}"
-    echo -e "\t${C}[${W}4${C}] Logging & Auditing ${Y}"
-    echo -e "\t${C}[${W}5${C}] Access, Authentication & Authorization ${Y}"
-    echo -e "\t${C}[${W}6${C}] System Maintenance ${Y}"
+    echo -e "\v\t${C}[${W}1${C}] Initial Setup ${NC}"
+    echo -e "\t${C}[${W}2${C}] Services ${NC}"
+    echo -e "\t${C}[${W}3${C}] Network Configuration ${NC}"
+    echo -e "\t${C}[${W}4${C}] Logging & Auditing ${NC}"
+    echo -e "\t${C}[${W}5${C}] Access, Authentication & Authorization ${NC}"
+    echo -e "\t${C}[${W}6${C}] System Maintenance ${NC}"
     echo -e "\t${C}[${W}0${C}] ${Y}Exit${NC}\n"
 
-    printf "${C}[${W}+${C}] Select section: ${NC}" 
+    echo -e "${C}[${W}+${C}] Select section: ${NC}" 
     read menu_opt
 
     case $menu_opt in 
